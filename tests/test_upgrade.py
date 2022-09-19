aad3304cd2c3c4cd7432e05817c725fd0b247008
Konrad Rokicki <rokickik@janelia.hhmi.org>
2022-09-19 14:45:28 -0400

    fixed test

--- tests/test_upgrade.py
+++ tests/test_upgrade.py
@@ -1,24 +1,24 @@
 #!/usr/bin/env python
 
+import rapidjson
 from neuronbridge import legacy_model
 from neuronbridge.upgrade_model import *
-import json 
 
 if __name__ == '__main__':
 
     def convert(filename, convert_lambda):
         with open("test_data/"+filename) as f:
-            obj = convert_lambda(json.load(f))
+            obj = convert_lambda(rapidjson.load(f))
         with open("test_data_v3/"+filename, "w") as w:
-            json.dump(obj.dict(exclude_unset=True, by_alias=True), w, indent=2)
+            write_json(obj, w)
 
     convert("em-body.json", lambda x: upgrade_em_lookup(legacy_model.EMImageLookup(**x)))
     convert("em-body-vnc.json", lambda x: upgrade_em_lookup(legacy_model.EMImageLookup(**x)))
     convert("mcfo-line.json", lambda x: upgrade_lm_lookup(legacy_model.LMImageLookup(**x)))
     convert("mcfo-line-vnc.json", lambda x: upgrade_lm_lookup(legacy_model.LMImageLookup(**x)))
-    convert("flyem-flylight.json", lambda x: upgrade_cds_matches(legacy_model.CDSMatches(**x)))
-    convert("flyem-flylight-vnc.json", lambda x: upgrade_cds_matches(legacy_model.CDSMatches(**x)))
-    convert("flylight-flyem.json", lambda x: upgrade_cds_matches(legacy_model.CDSMatches(**x)))
-    convert("flylight-flyem-vnc.json", lambda x: upgrade_cds_matches(legacy_model.CDSMatches(**x)))
-    convert("pppresult.json", lambda x: upgrade_ppp_matches(legacy_model.PPPMatches(**x)))
-    convert("pppresult-vnc.json", lambda x: upgrade_ppp_matches(legacy_model.PPPMatches(**x)))
+    convert("flyem-flylight.json", lambda x: upgrade_matches(legacy_model.CDSMatches(**x)))
+    convert("flyem-flylight-vnc.json", lambda x: upgrade_matches(legacy_model.CDSMatches(**x)))
+    convert("flylight-flyem.json", lambda x: upgrade_matches(legacy_model.CDSMatches(**x)))
+    convert("flylight-flyem-vnc.json", lambda x: upgrade_matches(legacy_model.CDSMatches(**x)))
+    convert("pppresult.json", lambda x: upgrade_matches(legacy_model.PPPMatches(**x)))
+    convert("pppresult-vnc.json", lambda x: upgrade_matches(legacy_model.PPPMatches(**x)))

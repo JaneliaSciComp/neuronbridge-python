7fe031c01b50369c35fd4aaafba1bf24b6c53b8c
Konrad Rokicki <rokickik@janelia.hhmi.org>
2024-01-20 11:08:42 -0500

    updated to validate newest data

--- neuronbridge/validate_ray.py
+++ neuronbridge/validate_ray.py
@@ -15,11 +15,10 @@ import neuronbridge.model as model
 import pydantic
 import ray
 
-default_data_version = "3.0.0"
+default_data_version = "3.1.1"
 max_logs = 0
 debug = False
 batch_size = 1000
-Ï = False
 
 def inc_count(counts, s, value=1):
     if s in counts:
@@ -36,10 +35,12 @@ def sum_counts(a, b):
     return d
 
 
-def error(counts, s, *tags):
+def error(counts, s, *tags, trace=None):
     inc_count(counts, s)
     if counts[s] < max_logs:
         print(f"{s}:", *tags, file=sys.stderr)
+        if trace:
+            print(trace)
     if counts[s] == max_logs:
         #print(f"Reached maximum logging count for '{s}'", file=sys.stderr)
         pass
@@ -78,14 +79,7 @@ def validate(counts, image, filepath):
 def validate_image(filepath, counts, publishedNames):
     with open(filepath) as f:
         obj = rapidjson.load(f)
-        try:
-            lookup = model.ImageLookup(**obj)
-        except TypeError:
-            print(f"error in file {filepath}; got object {obj}")
-            return
-        except pydantic.ValidationError as e:
-            error(counts, "Validation failed when loading ImageLookup", filepath)
-            return
+        lookup = model.ImageLookup(**obj)
 
         if not lookup.results:
             error(counts, f"No images", filepath)
@@ -105,10 +99,8 @@ def validate_image_dir(image_dir):
             filepath = root+"/"+filename
             try:
                 validate_image(filepath, counts, publishedNames)
-            except Exception as e:
-                trace = traceback.format_exc()
-                print("Error validating", filepath)
-                print(trace)
+            except pydantic.ValidationError as e:
+                error(counts, "Validation failed for image", filepath, trace=traceback.format_exc())
                 inc_count(counts, "Exceptions")
             inc_count(counts, "Items")
             inc_count(counts, "Elapsed", value=time.perf_counter()-tic)
@@ -154,10 +146,8 @@ def validate_matches(match_files, publishedNames=None):
     for filepath in match_files:
         try:
             validate_match(filepath, counts, publishedNames)
-        except Exception as e:
-            trace = traceback.format_exc()
-            print("Error validating", filepath)
-            print(trace)
+        except pydantic.ValidationError as e:
+            error(counts, "Validation failed for match", filepath, trace=traceback.format_exc())
             inc_count(counts, "Exceptions")
     return counts
 
@@ -211,52 +201,36 @@ if __name__ == '__main__':
         help='Run the Ray dashboard for debugging')
     parser.add_argument('--no-dashboard', dest='includeDashboard', action='store_false', \
         help='Do not run the Ray dashboard for debugging')
-    parser.add_argument('--log-lines', '-l', dest='logLines', type=int, default=0, 
-        help='Number of lines per error to print to stderr (default 0)')
-    parser.add_argument('--one-batch', dest='one_batch', action='store_false',
+    parser.add_argument('--max-logs', '-l', dest='max_logs', type=int, default=100, \
+        help='Number of instances per error to print to stderr (default 100)')
+    parser.add_argument('--one-batch', dest='one_batch', action='store_false', \
         help='Do only one batch of match validation (for testing)')
-    
+    parser.add_argument('--match', dest='match_file', type=str, default=None, \
+        help='Only validate the given match file')
+
     parser.set_defaults(validateImageLookups=True)
     parser.set_defaults(validateMatches=True)
     parser.set_defaults(includeDashboard=False)
     parser.set_defaults(one_batch=False)
-    
+
     args = parser.parse_args()
     data_version = args.data_version
-    max_logs = args.logLines
+    max_logs = args.max_logs
     one_batch = args.one_batch
-    if data_version == "3.0.0-alpha":
-        # For the alpha model, the directory structure was inherited from the older models.
-        # This can be deleted once we merge this code into the main branch.
-        image_dirs = [
-            f"/nrs/neuronbridge/v{data_version}/brain/mips/em_bodies",
-            f"/nrs/neuronbridge/v{data_version}/brain/mips/all_mcfo_lines",
-            f"/nrs/neuronbridge/v{data_version}/brain/mips/split_gal4_lines",
-            f"/nrs/neuronbridge/v{data_version}/vnc/mips/em_bodies",
-            f"/nrs/neuronbridge/v{data_version}/vnc/mips/gen1_mcfo_lines",
-            f"/nrs/neuronbridge/v{data_version}/vnc/mips/split_gal4_lines_published",
-        ]
-        match_dirs = [
-            f"/nrs/neuronbridge/v{data_version}/brain/cdsresults.final/flyem-vs-flylight",
-            f"/nrs/neuronbridge/v{data_version}/brain/cdsresults.final/flylight-vs-flyem",
-            f"/nrs/neuronbridge/v{data_version}/brain/pppresults/flyem-to-flylight.public",
-            f"/nrs/neuronbridge/v{data_version}/vnc/cdsresults.final/flyem-vs-flylight",
-        ]
-    else:
-        image_dirs = [
-            f"/nrs/neuronbridge/v{data_version}/brain/mips/embodies",
-            f"/nrs/neuronbridge/v{data_version}/brain/mips/lmlines",
-            f"/nrs/neuronbridge/v{data_version}/vnc/mips/embodies",
-            f"/nrs/neuronbridge/v{data_version}/vnc/mips/lmlines",
-        ]
-        match_dirs = [
-            f"/nrs/neuronbridge/v{data_version}/brain/cdmatches/em-vs-lm/",
-            f"/nrs/neuronbridge/v{data_version}/brain/cdmatches/lm-vs-em/",
-            f"/nrs/neuronbridge/v{data_version}/brain/pppmatches/em-vs-lm/",
-            f"/nrs/neuronbridge/v{data_version}/vnc/cdmatches/em-vs-lm/",
-            f"/nrs/neuronbridge/v{data_version}/vnc/cdmatches/lm-vs-em/",
-            f"/nrs/neuronbridge/v{data_version}/vnc/pppmatches/em-vs-lm/",
-        ]
+
+    image_dirs = [
+        f"/nrs/neuronbridge/v{data_version}/brain+vnc/mips/embodies",
+        f"/nrs/neuronbridge/v{data_version}/brain+vnc/mips/lmlines",
+    ]
+
+    match_dirs = [
+        f"/nrs/neuronbridge/v{data_version}/brain/cdmatches/em-vs-lm/",
+        f"/nrs/neuronbridge/v{data_version}/brain/cdmatches/lm-vs-em/",
+        f"/nrs/neuronbridge/v{data_version}/brain/pppmatches/em-vs-lm/",
+        f"/nrs/neuronbridge/v{data_version}/vnc/cdmatches/em-vs-lm/",
+        f"/nrs/neuronbridge/v{data_version}/vnc/cdmatches/lm-vs-em/",
+        f"/nrs/neuronbridge/v{data_version}/vnc/pppmatches/em-vs-lm/",
+    ]
 
     cpus = args.cores
     if cpus:
@@ -286,26 +260,30 @@ if __name__ == '__main__':
         publishedNames = set()
         counts, unfinished = {}, []
 
-        if args.validateImageLookups:
-            print("Validating image lookups...")
-            for image_dir in image_dirs:
-                unfinished.append(validate_image_dir.remote(image_dir))
-            while unfinished:
-                finished, unfinished = ray.wait(unfinished, num_returns=1)
-                for result in ray.get(finished):
-                    publishedNames.update(result['publishedNames'])
-                    counts = sum_counts(counts, result['counts'])
-            if debug: print(f"Indexed {len(publishedNames)} published names")
-
-        if args.validateMatches:
-            print("Validating matches...")
-            for match_dir in match_dirs:
-                unfinished.append(validate_match_dir.remote(match_dir, \
-                        publishedNames if args.validateImageLookups else None))
+        if args.match_file:
+            batch = [args.match_file]
+            ray.get(validate_matches.remote(batch))
+        else:
+            if args.validateImageLookups:
+                print("Validating image lookups...")
+                for image_dir in image_dirs:
+                    unfinished.append(validate_image_dir.remote(image_dir))
                 while unfinished:
                     finished, unfinished = ray.wait(unfinished, num_returns=1)
                     for result in ray.get(finished):
-                        counts = sum_counts(counts, result)
+                        publishedNames.update(result['publishedNames'])
+                        counts = sum_counts(counts, result['counts'])
+                if debug: print(f"Indexed {len(publishedNames)} published names")
+
+            if args.validateMatches:
+                print("Validating matches...")
+                for match_dir in match_dirs:
+                    unfinished.append(validate_match_dir.remote(match_dir, \
+                            publishedNames if args.validateImageLookups else None))
+                    while unfinished:
+                        finished, unfinished = ray.wait(unfinished, num_returns=1)
+                        for result in ray.get(finished):
+                            counts = sum_counts(counts, result)
 
     finally:
         print_summary("Validation complete. Issue summary:", counts)
